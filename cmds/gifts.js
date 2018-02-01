1d6db4d8ed58199150aa8497e5758621d36365a2
Damian <dc.spelen@gmail.com>
2018-02-01 16:25:27 +0100

    Create gifts.js

---
+++ cmds/gifts.js
@@ -0,0 +1,7 @@
+module.exports.run = async (bot, message, args) => {
+    
+message.channel.send("https://www.steamgifts.com/user/StephsFNA")
+}
+      module.exports.help = {
+        name: "gifts",
+      }
