2feb93a6b4f3823ce6714b5cafea86eda4ea3079
Damian <dc.spelen@gmail.com>
2018-02-10 19:09:49 +0100

    Update coffee.js

--- cmds/coffee.js
+++ cmds/coffee.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, message, args) => {
     
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(kUser) return message.channel.send(`${message.author} just gave ${kUser} a ☕`)
-    if(!kUser) return message.channel.send(`${message.author} laughed like a madman while slowly drinking the coffee for themself!`);
+    if(!kUser) return message.channel.send(`${message.author} laughed evilly while slowly drinking the coffee for themself!`);
 
     if (message.deletable) {
         message.delete().catch(() => {});
